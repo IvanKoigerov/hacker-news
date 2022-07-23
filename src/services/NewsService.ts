be3e0159e63539d3cfe52fc621a778ed8196f88e
Ivan Koigerov <heartsf0xs@gmail.com>
2022-07-23 18:11:14 +0300

    fix(NewsService): fix types

--- src/services/NewsService.ts
+++ src/services/NewsService.ts
@@ -1,21 +1,20 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react"
-import { IComments } from "../models/IComments"
-import { INews } from "../models/INews"
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react';
+import { IComments } from '../models/IComments';
+import { INews } from '../models/INews';
 
 export const newsApi = createApi({
-   reducerPath: 'NewsPath',
-   baseQuery: fetchBaseQuery({ baseUrl: 'https://api.hnpwa.com/v0/' }),
-   endpoints: (build) => ({
-      fetchAllNews: build.query<INews[], number>({
-         query: (list: number = 1) => ({
-            url: `newest/${list}.json`
-         })
+  reducerPath: 'NewsPath',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.hnpwa.com/v0/' }),
+  endpoints: (build) => ({
+    fetchAllNews: build.query<INews[], number>({
+      query: (list: number = 1) => ({
+        url: `newest/${list}.json`,
       }),
-      fetchNews: build.query<IComments, any>({
-         query: (id: any) => ({
-            url: `item/${id}.json`,
-         })
+    }),
+    fetchNews: build.query<IComments, string>({
+      query: (id: string) => ({
+        url: `item/${id}.json`,
       }),
-   })
-})
-
+    }),
+  }),
+});
