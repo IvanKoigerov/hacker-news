5f47c3f0c8b3145e1c15773565de285290d8387c
Ivan Koigerov <heartsf0xs@gmail.com>
2022-08-08 18:44:18 +0300

    fix

--- client/src/page/NewsPage.tsx
+++ client/src/page/NewsPage.tsx
@@ -1,34 +1,32 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import NewsList from '../components/NewsPage/NewsList';
 import Footer from '../components/Footer/Footer';
 import Header from '../components/Header/Header';
 import MainWrapper from '../components/MainWrapper';
-import { newsLocalApi } from '../services/NewsService';
+import {newsLocalApi} from '../services/NewsService';
 
 const NewsPage = () => {
-  // const { newsArr, isLoading, refetch, isFetching } = useNews();
-  const [load, setLoad] = useState(false);
+    const [load, setLoad] = useState(false);
 
-  const { data: news, isLoading: loading } = newsLocalApi.useFetchAllNewsQuery(1);
-  const handleLoad = () => {
-    // refetch();
-    setLoad(true);
-  };
+    const {data: newsArr, isLoading, refetch, isFetching} = newsLocalApi.useFetchAllNewsQuery(1);
+    const handleLoad = () => {
+        refetch();
+        setLoad(true);
+    };
 
-  // useEffect(() => {
-  //   setLoad(false);
-  // }, [isFetching]);
+    useEffect(() => {
+        setLoad(false);
+    }, [isFetching]);
 
-  return (
-    <>
-      <Header refetch={handleLoad} />
-      <MainWrapper>
-        <NewsList newsArr={news!} isLoading={loading} />
-        {/*<NewsList newsArr={newsArr!} isLoading={isLoading || (isFetching && load)} />*/}
-      </MainWrapper>
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <Header refetch={handleLoad}/>
+            <MainWrapper>
+                <NewsList newsArr={newsArr!} isLoading={isLoading || (isFetching && load)}/>
+            </MainWrapper>
+            <Footer/>
+        </>
+    );
 };
 
 export default NewsPage;
--- client/src/page/SelectPage.tsx
+++ client/src/page/SelectPage.tsx
@@ -1,45 +1,37 @@
-import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { newsLocalApi } from '../services/NewsService';
+import React, {useEffect, useState} from 'react';
+import {useParams} from 'react-router-dom';
+import {newsLocalApi} from '../services/NewsService';
 import Header from '../components/Header/Header';
 import MainWrapper from '../components/MainWrapper';
 import Footer from '../components/Footer/Footer';
 import Select from '../components/SelectPage/Select';
 
 const SelectPage = () => {
-  const { id } = useParams();
-  // const {
-  //     data: comment,
-  //     isLoading,
-  //     refetch,
-  //     isFetching,
-  // } = newsApi.useFetchNewsQuery(id!, {
-  //     pollingInterval: 60000,
-  // });
+    const {id} = useParams();
+    const {data: comment, isLoading, isFetching, refetch} = newsLocalApi.useFetchNewsQuery(id!, {
+        pollingInterval: 60000,
+    });
 
-  const { data: comment1, isLoading: loading } = newsLocalApi.useFetchNewsQuery(id!);
+    const [load, setLoad] = useState(false);
 
-  const [load, setLoad] = useState(false);
+    const handleLoad = () => {
+        refetch();
+        setLoad(true);
+    };
 
-  const handleLoad = () => {
-    // refetch();
-    setLoad(true);
-  };
+    useEffect(() => {
+        setLoad(false);
+    }, [isFetching]);
 
-  // useEffect(() => {
-  //     setLoad(false);
-  // }, [isFetching]);
-
-  return (
-    <>
-      <Header refetch={handleLoad} />
-      <MainWrapper>
-        {/*<Select isFetching={isLoading || (isFetching && load) || loading} comment={comment!}/>*/}
-        <Select isFetching={loading} comment={comment1!} />
-      </MainWrapper>
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <Header refetch={handleLoad}/>
+            <MainWrapper>
+                <Select isFetching={isLoading || (isFetching && load)} comment={comment!}/>
+            </MainWrapper>
+            <Footer/>
+        </>
+    );
 };
 
 export default SelectPage;
